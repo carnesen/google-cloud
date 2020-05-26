fdbce3feac053e6511a90305913e5664326c5555
Chris Arnesen <chris.arnesen@gmail.com>
2020-05-25 18:43:28 -0700

    Increase unit test timeout

--- src/asset.test.ts
+++ src/asset.test.ts
@@ -1,20 +1,26 @@
 import { Asset } from './asset';
 
+const TEN_SECONDS = 10000;
+
 describe(__filename, () => {
-  it('gcloud runs the gcloud cli', async () => {
-    class Foo extends Asset<null> {
-      public async testGcloud() {
-        const result = await this.gcloud({
-          cwd: '.',
-          args: ['version'],
-        });
-        expect(result['Google Cloud SDK']).toMatch(/.*\..*\..*/);
+  it(
+    'gcloud runs the gcloud cli',
+    async () => {
+      class Foo extends Asset<null> {
+        public async testGcloud() {
+          const result = await this.gcloud({
+            cwd: '.',
+            args: ['version'],
+          });
+          expect(result['Google Cloud SDK']).toMatch(/.*\..*\..*/);
+        }
       }
-    }
-    const foo = new Foo({
-      context: { projectId: 'foo', requireResolve: require.resolve },
-      props: null,
-    });
-    await foo.testGcloud();
-  });
+      const foo = new Foo({
+        context: { projectId: 'foo', requireResolve: require.resolve },
+        props: null,
+      });
+      await foo.testGcloud();
+    },
+    TEN_SECONDS,
+  );
 });
